8261ddbc53fee79be3891607ff4df54f6c273b5c
shota_kizawa <shomok0106k@gmail.com>
2021-07-26 15:09:34 +0900

    fix:  function in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -159,9 +159,8 @@ export function readSFC (target: string, ig: Ignore): SFCFileInfo[] {
 }
 
 function resolveGlob (target: string) {
-  const relativeTarget = path.relative(process.cwd(), target)
   // TODO: async implementation
-  return glob.sync(`${relativeTarget}/**/*.vue`)
+  return glob.sync(`${target}/**/*.vue`)
 }
 
 export const DEFUALT_CONF = { provider: {}} as ProviderConfiguration
@@ -431,7 +430,7 @@ export function splitLocaleMessages (
 
 export function readIgnoreFile (target: string, ignoreFileName: string): string[] {
   const ignoreFiles = glob.sync(`${target}/**/${ignoreFileName}`)
-  console.log(`allignore ${ignoreFiles}`)
+  console.log(`ignoreFiles ${ignoreFiles}`)
   const ignoreTargets = [] as string[]
   ignoreFiles.forEach(ignoreFile => {
     fs.readFileSync(ignoreFile, 'utf8')
