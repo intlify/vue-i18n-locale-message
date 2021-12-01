f1b57798af521791a9ebe7edc1331d1c0cb38446
kazuya kawaguchi <kawakazu80@gmail.com>
2021-12-01 17:29:19 +0900

    fix: undefined resource copy (#215)

--- src/utils.ts
+++ src/utils.ts
@@ -462,7 +462,7 @@ export function splitLocaleMessages (
     if (filename && isLocaleMessageDictionary(targetLocaleMessage)) {
       targetLocaleMessage = targetLocaleMessage[filename]
     }
-    meta.push({ path, messages: deepCopy(targetLocaleMessage) })
+    targetLocaleMessage && meta.push({ path, messages: deepCopy(targetLocaleMessage) })
     return meta
   }, [] as MetaExternalLocaleMessages[])
   debug('splitLocaleMessages: messages (after):', messages)
